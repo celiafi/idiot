5c8ad0837f77e83573007bcba21912bca4892f65
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-02-12 16:14:22 +0200

    initial commit

---
+++ src/DefaultEventProcessor.java
@@ -0,0 +1,290 @@
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchEvent.Kind;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class DefaultEventProcessor implements EventProcessor {
+
+	Idiot autoEncryptor;
+	Map<Path, Path> directories;
+	
+	public DefaultEventProcessor(Idiot autoEncryptor) {
+		this.autoEncryptor = autoEncryptor;
+		this.directories = new HashMap<Path, Path>();
+	}
+	
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void processEvent(WatchKey key, WatchEvent<?> event) {
+		Kind<?> kind = event.kind();
+
+		if (kind == OVERFLOW) {
+			Idiot.LOGGER
+					.warning("Overflow error. Manual check necessary.");
+		}
+
+		if (kind == ENTRY_CREATE) {
+			processCreation(key, event);
+		}
+	}
+
+	private void processCreation(WatchKey key, WatchEvent<?> event) {
+
+		Path dir = Idiot.keys.get(key);
+		Path remote = directories
+				.get(Idiot.keys.get(key));
+		Idiot.LOGGER.fine("Current watched directory: " + dir);
+		Idiot.LOGGER.fine("Current remote directory: " + remote);
+
+		WatchEvent<Path> ev = Idiot.cast(event);
+		Path name = ev.context();
+		Path pathToFile = dir.resolve(name);
+
+		Idiot.LOGGER.config(event.kind().name() + ": " + pathToFile);
+
+		// If directory, zip recursively
+		if (new File(pathToFile.toString()).isDirectory()) {
+			zipRecursively(pathToFile);
+		}
+
+		// If an ordinary file, encrypt
+		else {
+			encryptAndMoveFile(pathToFile, remote);
+		}
+
+	}
+
+	void encryptAndMoveFile(Path pathToFile, Path remote) {
+
+		String extension = getExtensionFromPath(pathToFile);
+		if (!extension.equals("axx")) {
+			waitUntilPathIsAccessible(pathToFile);
+			if (pathIsAccessible(pathToFile)) {
+				Idiot.LOGGER
+						.fine("File " + pathToFile + " accessible.");
+				try {
+					Path encrypted = encrypt(pathToFile);
+					Idiot.LOGGER.info("Encrypted " + encrypted
+							+ " succesfully.");
+					move(encrypted, remote);
+					Idiot.LOGGER.info("Moved " + encrypted + " to "
+							+ remote + " succesfully.");
+				} catch (IOException e) {
+					Idiot
+							.logExceptionAsSevere(
+									e,
+									"IO exception when moving the file. File "
+											+ "might already exist or the remote may "
+											+ "be inaccessible or not reachable due to for example network problems.");
+					return;
+				}
+			}
+		}
+	}
+
+	void waitUntilPathIsAccessible(Path path) {
+		while (!pathIsAccessible(path)) {
+			try {
+				Idiot.LOGGER.finest("File not accessible, sleeping.");
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				Idiot.logExceptionAsSevere(e,
+						"Interrupted while sleeping.");
+			}
+		}
+	}
+
+	boolean pathIsAccessible(Path path) {
+		String fileName = path.toString();
+		File file = new File(fileName);
+		File sameFileName = new File(fileName);
+		return file.renameTo(sameFileName);
+	}
+
+	void zipRecursively(Path pathToFile) {
+		Idiot.LOGGER.config("File " + pathToFile
+				+ " is directory, zipping.");
+
+		waitUntilPathIsAccessible(pathToFile);
+		if (pathIsAccessible(pathToFile)) {
+			Idiot.LOGGER.fine("File " + pathToFile + " accessible.");
+			try {
+				zipDirectory(pathToFile);
+				return;
+			} catch (IOException e) {
+				Idiot.logExceptionAsSevere(e,
+						"Something went wrong while zipping!");
+			}
+		}
+
+	}
+
+	private void zipDirectory(Path path) throws IOException {
+		String fileName = path.toString();
+		File file = new File(fileName);
+
+		String zipFileName = file.getCanonicalPath().concat(".zip");
+		ZipOutputStream out = new ZipOutputStream(new FileOutputStream(
+				zipFileName));
+		Idiot.LOGGER.config("Creating: " + zipFileName);
+		addDirToZip(file, out);
+		out.close();
+		Idiot.LOGGER.fine("Deleting directory " + file);
+		if (deleteDirectory(file)) {
+			Idiot.LOGGER.config("Deleted directory " + file
+					+ " succesfully.");
+		} else {
+			Idiot.LOGGER.config("Deleting directory " + file
+					+ " not succesful.");
+		}
+	}
+
+	private boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					Idiot.LOGGER.finest("Delete file " + files[i]);
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+
+	private void addDirToZip(File file, ZipOutputStream out)
+			throws IOException {
+		File[] files = file.listFiles();
+		byte[] tmpBuf = new byte[1024];
+
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].isDirectory()) {
+				addDirToZip(files[i], out);
+				continue;
+			}
+			FileInputStream in = new FileInputStream(files[i].getAbsolutePath());
+			Idiot.LOGGER.config("Adding to zip: "
+					+ files[i].getAbsolutePath());
+			String relativePath = file.toURI().relativize(files[i].toURI())
+					.toString();
+			out.putNextEntry(new ZipEntry(relativePath));
+			int len;
+			while ((len = in.read(tmpBuf)) > 0) {
+				out.write(tmpBuf, 0, len);
+			}
+			out.closeEntry();
+			in.close();
+		}
+	}
+
+	private void move(Path file, Path newLocation) throws IOException {
+		Idiot.LOGGER.finest("Getting filename for " + file);
+		Path filename = file.getFileName();
+		Idiot.LOGGER.finest("Got filename for " + file);
+		Idiot.LOGGER.finest("Getting new location " + newLocation);
+		newLocation = Paths.get(newLocation.toString().concat("\\")
+				.concat(filename.toString()));
+		Idiot.LOGGER.finest("Got new location " + newLocation);
+		Idiot.LOGGER.config("Moving file " + filename + " to "
+				+ newLocation);
+		Files.move(file, newLocation);
+	}
+
+	private String getExtensionFromPath(Path path) {
+		String extension = "";
+		int i = path.toString().lastIndexOf('.');
+		if (i > 0) {
+			extension = path.toString().substring(i + 1);
+		}
+		return extension;
+	}
+
+	private Path encrypt(Path pathToFile) throws IOException {
+
+		String commandString = createEncryptionStringForPath(pathToFile);
+		if (executeExternalCommand(commandString)) {
+			return getEncryptedFilePath(pathToFile);
+		} else {
+			Idiot.LOGGER.severe("Encryption of file " + pathToFile
+					+ " not succesful.");
+			throw (new IOException());
+		}
+	}
+
+	private String createEncryptionStringForPath(Path pathToFile) {
+		String encryptionString = "C:\\Program Files\\Axantum\\Axcrypt\\AxCrypt -b 2 -e -k "
+				+ "\""
+				+ autoEncryptor.passphrase
+				+ "\""
+				+ " -z "
+				+ "\""
+				+ pathToFile + "\"";
+		return encryptionString;
+	}
+
+	private boolean executeExternalCommand(String command)
+			throws IOException {
+		Process process;
+
+		process = Runtime.getRuntime().exec(command);
+		InputStream stream = process.getInputStream();
+		Reader reader = new InputStreamReader(stream);
+		BufferedReader bReader = new BufferedReader(reader);
+		String nextLine = null;
+		while ((nextLine = bReader.readLine()) != null) {
+			Idiot.LOGGER.config("Process output: " + nextLine);
+		}
+		int exitValue = process.exitValue();
+		Idiot.LOGGER.config("Process exited with value: " + exitValue);
+		if (exitValue == 0) {
+			return true;
+		} else {
+			Idiot.LOGGER.severe("External command not succesful.");
+			return false;
+		}
+	}
+
+	private Path getEncryptedFilePath(Path path) {
+		String extension = getExtensionFromPath(path);
+		String pathString = path.toString();
+
+		int lastIndex = pathString.lastIndexOf(extension);
+		int dotIndex = lastIndex;
+		if (!extension.equals(""))
+			dotIndex = lastIndex - 1;
+		String pathNoExt = pathString.substring(0, dotIndex);
+
+		String newPath;
+		if (!extension.equals(""))
+			newPath = pathNoExt + "-" + extension + ".axx";
+		else
+			newPath = pathNoExt + ".axx";
+
+		path = Paths.get(newPath);
+		return path;
+	}
+
+}
---
+++ src/Idiot.java
@@ -0,0 +1,197 @@
+import java.nio.file.*;
+import java.io.File;
+import java.io.IOException;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Idiot {
+	private WatchService watcher;
+	private ArrayList<EventProcessor> eventProcessors;
+	static Map<WatchKey, Path> keys;
+
+	String passphrase;
+
+	public final static Logger LOGGER = Logger.getLogger(Idiot.class.getName());
+	private Level logLevel;
+	private static FileHandler fh;
+	private static SimpleFormatter formatter;
+	private Properties config;
+
+	@SuppressWarnings("unchecked")
+	static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+		return (WatchEvent<T>) event;
+	}
+
+	public Idiot() throws IOException {
+
+		readConfig();
+		initLogger();
+
+		this.eventProcessors = new ArrayList<EventProcessor>();
+		eventProcessors.add(new DefaultEventProcessor(this));
+
+		this.logLevel = Level.parse(config.getProperty("logLevel"));
+		LOGGER.setLevel(logLevel);
+		LOGGER.info("Log level " + logLevel);
+
+		this.watcher = FileSystems.getDefault().newWatchService();
+		keys = new HashMap<WatchKey, Path>();
+
+		passphrase = config.getProperty("passphrase");
+
+		register();
+		initEventProcessors();
+	}
+
+	private void initLogger() {
+		try {
+			fh = new FileHandler(config.getProperty("logLocation"));
+			LOGGER.addHandler(fh);
+			formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+		} catch (SecurityException e) {
+			LOGGER.severe("Security exception during initialization.");
+			LOGGER.severe(e.getStackTrace().toString());
+		} catch (IOException e) {
+			LOGGER.severe("IO exception during initialization.");
+			LOGGER.severe(e.getStackTrace().toString());
+		}
+	}
+
+	private void readConfig() throws IOException {
+		this.config = new Properties();
+		InputStream in;
+
+		// TODO test if external properties overrides internal defaults w/ same
+		// file name
+		in = this.getClass().getResourceAsStream("idiot.properties");
+
+		config.load(in);
+		in.close();
+	}
+
+	private void initEventProcessors() {
+		for (int i = 0; i < eventProcessors.size(); i++) {
+			eventProcessors.get(i).initialize();
+		}
+	}
+
+	// TODO this is ugly, find a generic solution for all EventProcessors.
+	private void register() throws IOException {
+
+		int i = 1;
+		String p;
+		while ((p = config.getProperty("watchDir" + i)) != null) {
+			Path watchDir = Paths.get(p);
+			if (config.getProperty("remoteDir" + i) != null) {
+				Path remoteDir = Paths.get(config.getProperty("remoteDir" + i));
+				LOGGER.info("Watching " + watchDir + ", - Remote is "
+						+ remoteDir);
+
+				// TODO ughhhh
+				for (int j = 0; j < eventProcessors.size(); j++) {
+					EventProcessor ep = eventProcessors.get(j);
+					if (ep instanceof DefaultEventProcessor) {
+						DefaultEventProcessor dep = (DefaultEventProcessor) ep;
+						dep.directories.put(watchDir, remoteDir);
+					}
+				}
+
+				registerDirectory(watchDir);
+				i++;
+			} else {
+				LOGGER.severe("The amount of remote directories is not equivalent "
+						+ "to the amount of watched directories! "
+						+ "Please check your configuration file.");
+				break;
+			}
+			LOGGER.fine("Watcher created succesfully.");
+		}
+	}
+
+	private void registerDirectory(Path dir) throws IOException {
+		LOGGER.config("Registering directory " + dir);
+		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,
+				ENTRY_MODIFY);
+		keys.put(key, dir);
+	}
+
+	private void processEvents() {
+
+		LOGGER.info("Initialization succesful!");
+		for (;;) {
+
+			WatchKey key;
+
+			try {
+				key = getWatchKey();
+			} catch (InterruptedException e) {
+				logExceptionAsSevere(e, "Watcher interrupted.");
+				return;
+			}
+
+			processEvents(key);
+
+			// reset key and remove from set if directory no longer accessible
+			boolean valid = key.reset();
+			if (!valid) {
+				LOGGER.severe("Directory key " + key
+						+ " is no longer accessible. Removing from keys.");
+				keys.remove(key);
+
+				if (keys.isEmpty()) {
+					LOGGER.severe("All directories are inaccessible. Halting.");
+					break;
+				}
+			}
+		}
+	}
+
+	static void logExceptionAsSevere(Exception e, String message) {
+		LOGGER.severe(message);
+		LOGGER.severe(e.toString());
+		LOGGER.severe(e.getMessage());
+		return;
+	}
+
+	private WatchKey getWatchKey() throws InterruptedException {
+		WatchKey key = watcher.take();
+		LOGGER.finest("Watch key: " + key);
+		return key;
+	}
+
+	/**
+	 * Process events for one key.
+	 * 
+	 * @param key
+	 */
+	private void processEvents(WatchKey key) {
+		if (!directoryIsNull(key)) {
+			for (WatchEvent<?> event : key.pollEvents()) {
+				for (EventProcessor processor : eventProcessors) {
+					processor.processEvent(key, event);
+				}
+			}
+		}
+	}
+
+	private boolean directoryIsNull(WatchKey key) {
+		if (keys.get(key) == null) {
+			LOGGER.warning("Watched directory is non-existent or not recognized.");
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Idiot().processEvents();
+	}
+
+}
